2e82b90b21e4343d74889dedbd16c87c45dc4ce7
ProGM <progiemmeh@gmail.com>
2018-03-22 16:39:14 +0000

    Update docs

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import LocText from './ui/loc-text';
-import Drag from './utils/drag';
-import Geom from './ui/geom-spr';
-import RectOverlay from './ui/rectangle-overlay';
-import CoordinatesHelper from './utils/coordinates-helper';
-
-export { LocText, Drag, CoordinatesHelper, Geom, RectOverlay };
